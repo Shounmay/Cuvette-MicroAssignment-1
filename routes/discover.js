51e72282299f938e87c047ee7e3bdc438204ce58
Shounmay <shounmaydarsh131@gmail.com>
2022-12-17 20:57:54 +0530

    added discover images api

--- routes/discover.js
+++ routes/discover.js
@@ -7,11 +7,31 @@ route.get('/get-all-categories', async (req, res, next) => {
 	try {
 		let categoryList = [];
 		AllCategoryDetails = await CategoryModel.find({});
-		console.log();
+
 		await AllCategoryDetails.forEach((category) => {
 			categoryList.push(category.name);
 		});
 		res.send(categoryList);
+	} catch {
+		console.log(error);
+		next(error);
+	}
+});
+
+route.get('/:category/:shuffle', async (req, res, next) => {
+	try {
+		const category = req.params.category;
+		const shuffle = req.params.shuffle;
+
+		if (!category) {
+			res.status(400).send('Bad Request');
+		}
+
+		const galleryDetails = await GalleryModel.find({
+			category: { $in: [category] },
+		}).limit(4);
+
+		res.json(galleryDetails);
 	} catch {
 		console.log(error);
 		next(error);
