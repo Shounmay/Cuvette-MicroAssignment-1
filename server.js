43722c862563b9c48e4c1c958ee326c9dde9b72c
Shounmay <shounmaydarsh131@gmail.com>
2022-12-17 16:18:19 +0530

    added create a category api

--- server.js
+++ server.js
@@ -3,8 +3,26 @@ dotenv.config();
 const express = require('express');
 const initializeDatabase = require('./config/db');
 const app = express();
+const admin = require('./routes/admin');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 initializeDatabase();
+
+app.get('/api/health', (req, res) => {
+	res.send({
+		time: new Date(),
+		server: 'Shuffle Backend',
+		status: 'Active',
+	});
+});
+
+app.use('/api/admin', admin);
+
+app.use((req, res, next) =>
+	res.status(404).send('You are looking for something that we not have!')
+);
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.host || 'localhost';
