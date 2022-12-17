3358efde208bf70653b5843ffbd85c75ef6ff1c9
Shounmay <shounmaydarsh131@gmail.com>
2022-12-17 16:23:41 +0530

    added error handler middleware

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use((req, res, next) =>
 	res.status(404).send('You are looking for something that we not have!')
 );
 
+app.use((err, req, res, next) => res.status(500).send('Something went wrong!'));
+
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.host || 'localhost';
 
